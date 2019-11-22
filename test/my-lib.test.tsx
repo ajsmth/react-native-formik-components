c618807b99d58748ee13fe2fe98bfe1ad458a6c1
andy <andyboythekid@gmail.com>
2019-11-21 18:28:40 -0700

    fix tests

--- test/my-lib.test.tsx
+++ test/my-lib.test.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
-import { render } from '@testing-library/react-native'
-import { MyLib } from '../src'
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import { Input, Form, DebugForm } from '../src';
+import { Formik } from 'formik';
+
+function MyForm() {
+  return (
+    <Formik
+      initialValues={{ email: '', firstName: '', password: '' }}
+      onSubmit={console.log}
+    >
+      <Form>
+        {() => {
+          return (
+            <>
+              <Input name="email" type="email" placeholder="Email" />
+              <Input name="firstName" type="name" placeholder="Name" />
+              <Input name="password" type="password" placeholder="Password" />
+              <DebugForm />
+            </>
+          );
+        }}
+      </Form>
+    </Formik>
+  );
+}
 
 test('render()', () => {
-  const { debug } = render(<MyLib />)
+  const { debug } = render(<MyForm />);
 
-  debug()
-})
+  debug();
+});
